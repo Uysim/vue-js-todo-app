73f6c9ee9991535299ef6b88d2acfaea28fc04a8
Uysim Ty <uysimty@gmail.com>
2018-07-24 11:32:23 +0700

    list edit and delete

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class='ui centered card' >
+    <div class='content' v-show='!isEditing'>
+      <div class='header'>
+        {{ todo.title }}
+      </div>
+      <div class='meta'>
+        {{ todo.project }}
+      </div>
+      <div class='extra content'>
+        <a class='right floated trash icon' v-on:click='deleteTodo(index)'>
+          <i class='trash red icon'></i>
+        </a>
+        <a class='right floated edit icon' v-on:click='showForm'>
+          <i class='edit icon'></i>
+        </a>
+
+      </div>
+    </div>
+
+    <div class='content' v-show='isEditing'>
+      <div class='ui form'>
+        <div class='field'>
+          <label>
+            Title
+          </label>
+          <input type="text" v-model="todo.title">
+        </div>
+        <div class='field'>
+          <label>
+            Project
+          </label>
+          <input type="text" v-model="todo.project">
+        </div>
+        <div class='ui two button attached buttons'>
+          <button class='ui basic blue button' v-on:click='hideForm'>
+            Close X
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class='ui buttom attached green basic button' v-show='!isEditing && todo.done'>
+      Completed
+    </div>
+    <div class='ui buttom attached red basic button' v-show='!isEditing && !todo.done'>
+      Pending
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['todo'],
+  data () {
+    return {
+      isEditing: false
+    }
+  },
+  methods: {
+    deleteTodo (index) {
+      this.$emit('delete-todo', index)
+    },
+    showForm () {
+      this.isEditing = true;
+    },
+    hideForm() {
+      this.isEditing = false;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p>
+      Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}
+    </p>
+    <p>
+      Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}
+    </p>
+    <todo-item  v-on:delete-todo="deleteTodo" v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:index="index"></todo-item>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+export default {
+  name: 'TodoList',
+  components: {
+    TodoItem
+  },
+
+  methods: {
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
+  }
+}
+</script>
+
+<style></style>
